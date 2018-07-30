be616687736be37f51a31d249bd175fc09fca872
yazid farhaoui <yazid.zibem@gmail.com>
2018-07-30 17:42:07 +0200

    exo4

--- src/test/java/java8/ex01/Optional_01_Test.java
+++ src/test/java/java8/ex01/Optional_01_Test.java
@@ -4,6 +4,7 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.nio.file.DirectoryStream.Filter;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -20,9 +21,9 @@ public class Optional_01_Test {
 
 
     // tag::findMethod[]
-    <T> T find(List<T> list, Predicate<T> predicate) {
-        T result = null;
-
+    <T> T find(List<T> list, Predicate<T> predicate, T defaultValue) {
+        T result = defaultValue;
+        
         for (T p : list) {
             if (predicate.test(p)) {
                 result = p;
@@ -39,10 +40,10 @@ public class Optional_01_Test {
     public void test_optional_found() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
+        Predicate<Person> predicate = (p)-> p.getAge() == 10;
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.of(find(personList, predicate,null));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(true));
@@ -56,10 +57,11 @@ public class Optional_01_Test {
 
 
         List<Person> personList = Data.buildPersonList(100);
+        Predicate<Person> predicate = (p)-> p.getAge() == 400;
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 400
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.ofNullable(find(personList, predicate,null));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(false));
@@ -70,10 +72,12 @@ public class Optional_01_Test {
 
 
         List<Person> personList = Data.buildPersonList(100);
+        Predicate<Person> predicate = (p)-> p.getAge() == 10 && p.getFirstname().equals("last_10");
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10 et firstname == "last_10"
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.ofNullable(find(personList, predicate,null));
+        		result.orElseThrow(() -> new NotFountException());
 
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception NotFountException si non trouvé
     }
@@ -87,10 +91,12 @@ public class Optional_01_Test {
         Person defaultValue = new Person();
         defaultValue.setFirstname("DEFAULT");
         defaultValue.setLastname("DEFAULT");
+        
+        Predicate<Person> predicate = (p)-> p.getAge() == 400 ;
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate, T defaultValue)
         // TODO predicate => age == 400
-        Person result = null;
+        Person result = find(personList, predicate,defaultValue);
 
         assertThat(result, notNullValue());
         assertThat(result, hasProperty("firstname", is("DEFAULT")));
--- src/test/java/java8/ex02/Optional_02_Test.java
+++ src/test/java/java8/ex02/Optional_02_Test.java
@@ -4,6 +4,7 @@ package java8.ex02;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.nio.file.DirectoryStream.Filter;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -29,13 +30,13 @@ public class Optional_02_Test {
 
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
+        Optional<Person> julesOpt = Optional.of(jules);
 
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "adult"
-        Optional<Person> adultPerson = null;
+        Optional<Person> adultPerson =  julesOpt.filter(adult);
 
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "aged"
-        Optional<Person> agedPerson = null;
+        Optional<Person> agedPerson = julesOpt.filter(aged);;
 
         assertThat(adultPerson.isPresent(), is(true));
         assertThat(agedPerson.isPresent(), is(false));
@@ -47,10 +48,10 @@ public class Optional_02_Test {
 
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
+        Optional<Person> julesOpt = Optional.of(jules);
 
         // TODO récupérer l'age de jules via la méthode "map"
-        Optional<Integer> julesAge = null;
+        Optional<Integer> julesAge = julesOpt.map( (t)-> t.getAge() );
 
         assertThat(julesAge.isPresent(), is(true));
         assertThat(julesAge.get(), is(30));
@@ -60,14 +61,25 @@ public class Optional_02_Test {
     @Test(expected = GoodException.class)
     public void test_optional_ifPresent() throws Exception {
         Person jules = new Person("Hugues", "Jules", 30, "pass");
-
+        
+        
         // TODO encapsuler la valeur jules dans un type Optional
         // TODO utiliser la méthode "of"
-        Optional<Person> julesOpt = null;
-
+        Optional<Person> julesOpt = Optional.of(jules);
+        
+        Optional<Integer> julesAge = julesOpt.filter(adult)
+        		.map((p)-> p.getAge())
+        		;
+       
+        julesAge.ifPresent((i) -> {
+        	assertThat(i, is(30));
+        	throw new GoodException();
+        });
+        
         // TODO appliquer la méthode "filter" à julesOpt avec le prédicat "adult"
         // TODO chaîner avec la méthode "map" pour récupérer l'age
-        // TODO utiliser la méthode isPresent pour vérifier que l'age est bien 30, déclencher l'exception GoodException pour valider que la fonction en paramètre de ifPresent a bien été exécutée.
+        // TODO utiliser la méthode ifPresent pour vérifier que l'age est bien 30, déclencher 
+        //l'exception GoodException pour valider que la fonction en paramètre de ifPresent a bien été exécutée.
         // julesOpt.filter...;
     }
 }
